8ce667c96a237fb859249844d18ed72bb6bff848
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-02 15:55:40 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,8 +3,9 @@
 * print a string
 */
 int main(void)
+
 {
-    char *s = "\"Programming is like building a multilingual puzzle";
-    puts(s);
-    return(0);
+char *s = "\"Programming is like building a multilingual puzzle";
+puts(s);
+return(0);
 }
