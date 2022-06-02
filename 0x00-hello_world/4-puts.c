4e00eb3fcbda264a43dfd5fac0f451f93e685cb6
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-02 16:08:15 -0700

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
 /*
 * main-entry
-*
+(*
 *Return: 0
+(*
+*my_function - kklk kkkhj
 */
 int main(void)
-/*
-* main-entry
-*
-*Return: 0
-*/
+
 {
 char *s = "\"Programming is like building a multilingual puzzle";
 puts(s);
