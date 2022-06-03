87e4347f369355d983b8f70d55eaa95fda471ca8
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-03 15:20:08 -0700

    Update 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
+int main(void)
+
+{
+int a;
+for (a = 0; a < 0; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
