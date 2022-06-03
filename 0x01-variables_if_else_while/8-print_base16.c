cb6f1deb3df4e7ad88301722e587f7079c0f4e55
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-03 16:38:11 -0700

    Update 8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
+int main(void)
+
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+putchar((a % 10) + '0');
+}
+for (alpha = 'a'; alpha <= 'f'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
