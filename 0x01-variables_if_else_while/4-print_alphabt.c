0b6d4c65e4492527e7c1c130b27f2b52bb0fc643
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-03 14:26:58 -0700

    Update 4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
+int main(void)
+
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z' && alpha != e || q; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+return (0);
+}
