897f280eb977df30ea8ec1ee7893d215f85da2eb
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-02 17:14:15 -0700

    Update 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
+int main(void)
+
+{
+fprintf(stderr,"and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+}
