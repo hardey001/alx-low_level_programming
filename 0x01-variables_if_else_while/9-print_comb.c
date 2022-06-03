9ffb4c331d7990ee60a142411ca1518634228269
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-03 15:18:58 -0700

    jhjh

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
+int main(void)
+
+{
+int a;
+for (a = 0; a < 0; a++)
+{
+putchar(a);
+putchar(",");
+putchar(" ");
+}
+putchar('\n');
+return (0);
+}
