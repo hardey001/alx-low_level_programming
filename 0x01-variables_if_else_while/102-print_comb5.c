15c3dcefd5e31c0b8f773191dbc13ae888d0732b
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-03 17:03:39 -0700

    Update 102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
+int main(void)
+
+{
+int a;
+int b;
+for (a = 0; a < 10; a++)
+{
+for (b = 0; b < 10; b++)
+{
+putchar((a % 10) + '0');
+putchar((b % 10) + '0');
+if (a == 9 && b == 9)
+{
+continue;
+}
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
