461fefbec06886104b6f32f38e22034e103eadb1
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-03 17:14:08 -0700

    Update 102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,16 +11,24 @@ int main(void)
 {
 int a;
 int b;
+int c;
+int d;
 for (a = 0; a < 10; a++)
 {
 for (b = 0; b < 10; b++)
 {
-putchar((0 % 10) + '0');
-putchar((0 % 10) + '0');
-putchar(' ');
 putchar((a % 10) + '0');
 putchar((b % 10) + '0');
-if (a == 9 && b == 9)
+putchar(' ');
+for (c = 0; c < 10; c++)
+{
+for (d = 0; d < 10; d++)
+{
+putchar((c % 10) + '0');
+putchar((d % 10) + '0');
+}
+}
+if (a == 9 && b == 8 && c == 9 && d == 9)
 {
 continue;
 }
