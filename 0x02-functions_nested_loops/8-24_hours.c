4f3aff46ea21c39df928c974e5f182e22b4b52ed
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 10:26:15 -0700

    Update 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* jack_bauer - print alphabeth
+*/
+void jack_bauer(void)
+{
+int a;
+int b;
+for (a = 0; a <= 23; a++)
+{
+for (b = a + 1; b <= 59; b++)
+{
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
+_putchar(':');
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
+if (a == 23 && b == 59)
+{
+continue;
+}
+_putchar('\n');
+}
+}
+return (0);
+}
