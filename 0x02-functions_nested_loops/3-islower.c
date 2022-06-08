85f3308651e97c71d000c555f0573df932db6da1
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 06:40:29 -0700

    Update 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include <stdio>
+#include "main"
+/**
+* main - check the code.
+*
+* Return: Always 0.
+*/
+int _islower(int c)
+
+{
+if (islower(c) == 0)
+{
+return (0)
+}
+else
+{
+return (1)
+}
+}
