8ee38b90420d3c0208f9680efa156fff551fee0f
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 06:00:37 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,10 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* main-entry
-(*
-*Return: 0
-(*
-*my_function - kklk kkkhj
-*/
+ * my_function - print alphabethd
+ */
 void print_alphabet(void)
-/*hhfhhf*/
+
 {
 char alpha;
 for (alpha = 'a'; alpha <= 'z'; alpha++)
