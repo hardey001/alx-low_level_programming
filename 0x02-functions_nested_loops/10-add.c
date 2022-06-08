2d61f4d68381e833811569e95467e0131b221ebc
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 15:19:55 -0700

    vool

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* add - Short description, single line
+* @n: a
+* @b:lo
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int add(a, b)
+{
+printf("%d",(a + b));
+return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 (*
 *my_function - kklk kkkhj
 */
+int add(int, int);
 void times_table(void);
 void jack_bauer(void);
 int print_last_digit(int);
