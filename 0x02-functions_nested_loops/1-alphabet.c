aedfa19525e1979ccb5581cc170e9a978cf4d800
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 05:56:44 -0700

    cool

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -8,7 +8,13 @@
 *my_function - kklk kkkhj
 */
 void print_alphabet(void)
-
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
 {
 char alpha;
 for (alpha = 'a'; alpha <= 'z'; alpha++)
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,9 @@
-
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
 void print_alphabet();
 
