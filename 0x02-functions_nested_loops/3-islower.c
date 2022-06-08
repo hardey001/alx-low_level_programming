bba546c3506234a2d23e117b140e05201936bfed
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 07:45:41 -0700

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,9 +2,12 @@
 #include "main.h"
 #include <ctype.h>
 /**
-* main - check the code.
-*
-* Return: Always 0.
+* _islower - Short description, single line
+* @c: c
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
 */
 int _islower(int c)
 
