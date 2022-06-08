fc546b92b2af42b4eb745496db2e8c9b47cc9732
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 07:49:45 -0700

    check

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+#include <ctype.h>
+/**
+* _isalpha - Short description, single line
+* @c: c
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int _isalpha(int c)
+
+{
+
+if (isalpha(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 (*
 *my_function - kklk kkkhj
 */
+int _isalpha(int c);
 int _islower(int c);
 int _putchar(char c);
 void print_alphabet();
