37d01b3ee4af2ca7b7e4df11c8e2885540f89347
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 06:12:35 -0700

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* function_name - Short description, single line
-*Description of parameter x
-(* a blank line
-* Description: Longer description of the function)?
-(* section header: Section description)*
-* Return: Description of the returned value
+* my_function - print alphabeth
 */
 void print_alphabet(void)
 
