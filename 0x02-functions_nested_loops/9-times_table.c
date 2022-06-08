b26f560e695d4ccceb156321e6397db05f582a3b
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 13:51:59 -0700

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,14 +8,20 @@ void times_table(void)
 {
 int a;
 int b;
+int c;
 for(a = 0; a <= 9; a++)
 {
 for(b = 0; b <= 9; b++)
 {
-printf("%d", (a*b));
-if (b != 9)
+c= a * b;
+printf("%d", c);
+if (c < 9)
 {
 printf(",  ");
+}
+else
+{
+printf(", ");
 }
 if (a == 9 && b == 9)
 {
