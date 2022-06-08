a2c5eba1e2a21b38857e96a03136b7aeae2164f3
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 08:26:51 -0700

    flee

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include <stdio>
+#include "main"
+/**
+* print_sign - Short description, single line
+* @n: c
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int _abs(int n)
+{
+return (abs(n));
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 (*
 *my_function - kklk kkkhj
 */
+int _abs(int);
 int print_sign(int n);
 int _isalpha(int c);
 int _islower(int c);
