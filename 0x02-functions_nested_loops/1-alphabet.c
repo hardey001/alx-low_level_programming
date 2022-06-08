2ec1e842189c9b95d98225113999e0bf92ee0b05
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 05:26:01 -0700

    Update 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
+int main(void)
+
+{
+char alpha;
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+_putchar(alpha);
+}
+_putchar('\n');
+return (0);
+}
