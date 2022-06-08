6e9032e4bf7ead017b29cdef0dadc1395c9a816b
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 15:37:57 -0700

    fix

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* _abs - Short description, single line
+* @n: c
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+void print_to_98(int n)
+{
+int a;
+for(a = 0; a <= n; a++)
+{
+printf("%d, ",n);
+}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 (*
 *my_function - kklk kkkhj
 */
+void print_to_98(int n);
 int add(int, int);
 void times_table(void);
 void jack_bauer(void);
