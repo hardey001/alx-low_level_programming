552190b3d7db8703e50f03b8b465a4a8298e333f
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 08:36:12 -0700

    last

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main"
+/**
+* print_last_digit - Short description, single line
+* @y: c
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int print_last_digit(int y)
+{
+return (y % 10);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -5,6 +5,7 @@
 (*
 *my_function - kklk kkkhj
 */
+int print_last_digit(int);
 int _abs(int);
 int print_sign(int n);
 int _isalpha(int c);
