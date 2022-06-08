3ba8cd8b7c2c39657a2b93a36b1461eba5031e97
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-08 06:22:24 -0700

    Update 2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet_x10 - print alphabeth
+*/
+void print_alphabet_x10(void)
+
+{
+char alpha;
+for (i = 0; i < 10; 1++)
+{
+for (alpha = 'a'; alpha <= 'z'; alpha++)
+{
+putchar(alpha);
+}
+putchar('\n');
+}
+}
