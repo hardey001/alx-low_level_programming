344350e294cec1ece551bfdd89312a6b2dd0c3d8
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-09 15:57:11 -0700

    kiloo

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+* print_alphabet - print alphabeth
+*/
+void print_most_numbers(void)
+
+{
+int a;
+for (a = 0; a < 10; a++)
+{
+if (a != 4)
+{
+if (a != 2)
+{
+putchar((a % 10) + '0');
+}
+}
+}
+putchar('\n');
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,6 +5,8 @@
 (*
 *my_function - kklk kkkhj
 */
+void print_most_numbers(void);
 void print_numbers(void);
 int mul(int a, int b);
+int _isdigit(int c);
 int _isupper(int c);
