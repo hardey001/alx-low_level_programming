c15a49d212f5c259d2e710f80254cf5322327ff1
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-09 01:20:52 -0700

    Update 102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - Short description, single line
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+int num1 = 1,num2 = 2,num3,sum = 0;
+printf("%d, %d, ", num1, num2);
+for (i = 3; i < 50; i++)
+{
+num3 = num1 + num2;
+printf("%d,  ", num3);
+num1 = num2,
+num2 = num3;
+}
+}
