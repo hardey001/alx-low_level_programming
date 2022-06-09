134723c0ca65e8ebe2d9a8f7e8c12e05cd403143
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-09 01:01:18 -0700

    Update 101-natural.c

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,14 +8,14 @@
 */
 int main(void)
 {
-int i,sum = 0;
-for( i= 0;i < 1024;i++)
+int i, sum = 0;
+for(i= 0;i < 1024;i++)
 {
-if((i % 5 == 0)||(i % 3 == 0))
+if((i % 5 == 0) || (i % 3 == 0))
 {
-sum=sum + i;
+sum= sum + i;
 }
 }
-printf("%d", sum);
-return(0);
+printf("%d\n", sum);
+return (0);
 }
