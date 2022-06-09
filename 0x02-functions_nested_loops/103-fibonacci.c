233d88366b30b4546f21bf165ea2bb03e1850a43
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-09 01:41:12 -0700

    Update 103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+* main - Short description, single line
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+long num1 = 1, num2 = 2, num3, sum, i;
+for (i = 3; num3 <= 4000000; i++)
+{
+num3 = num1 + num2;
+if (num3 % 2 == 0)
+{
+sum = sum + num3;
+}
+}
+printf("%ld\n", (sum + 2));
+return (0);
+}
