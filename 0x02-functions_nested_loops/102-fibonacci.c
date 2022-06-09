83695b187fe82eec3c3ae4e1128bffae0ef2f953
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-09 01:23:36 -0700

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,7 +8,7 @@
 */
 int main(void)
 {
-int num1 = 1,num2 = 2,num3,sum = 0, i;
+int num1 = 1, num2 = 2, num3, i;
 printf("%d, %d, ", num1, num2);
 for (i = 3; i < 50; i++)
 {
@@ -17,4 +17,5 @@ printf("%d,  ", num3);
 num1 = num2,
 num2 = num3;
 }
+return (0);
 }
