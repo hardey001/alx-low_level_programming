6ed26c819a99ec2cc8f7e766cc21eb4ec8d1f2a2
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-09 02:44:26 -0700

    Update 104-fibonacci.c

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,24 @@
+##include <stdio.h>
+/**
+* main - Short description, single line
+(* a blank line
+* Description: Longer description of the function)?
+(* section header: Section description)*
+* Return: Description of the returned value
+*/
+int main(void)
+{
+long double num1 = 1, num2 = 2, num3, i;
+printf("%Lf, %Lf, ", num1, num2);
+for (i = 3; i <= 98; i++)
+{
+num3 = num1 + num2;
+if (i == 50)
+printf("%Lf\n", num3);
+else
+printf("%Lf, ", num3);
+num1 = num2;
+num2 = num3;
+}
+return (0);
+}
