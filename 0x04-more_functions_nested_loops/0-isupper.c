9a199d92bcb44585637a9671ce31f9917acdfd6b
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-09 15:00:45 -0700

    add

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isupper: returnnmm
+ * @c: add am 
+ * Return: 0
+ */
+
+int _isupper(int c)
+
+{
+if (isupper(c))
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
