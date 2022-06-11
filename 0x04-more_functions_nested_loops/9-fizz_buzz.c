8f221f085501e747114c7e80a078d1c3a9658a57
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-11 12:42:29 -0700

    Create 9-fizz_buzz.c

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+* main-entry
+(*
+*Return: 0
+(*
+*my_function - kklk kkkhj
+*/
+int main(void)
+{
+int i;
+for (i=1; i < 100; i++)
+{
+if (i % 15 ==0)
+printf("FizzBuzz\t");
+else if ((i % 3) == 0)
+{
+printf("Fizz\t");
+}
+else if ((i % 5) == 0)
+{
+printf("Buzz\t");
+}
+else
+printf("%d\t", i);
+}  
+}
