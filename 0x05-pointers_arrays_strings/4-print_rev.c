d351648ca8d381e807a10b0f84780b4d3bc418ad
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-14 10:45:50 -0700

    hello

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,6 +7,18 @@
 */
 
 void print_rev(char *s)
+
 {
-printf("%s\n", strrev(*s));
+int n = _strlen(*s), i;
+char *begin_ptr, *end_ptr, ch;
+begin_ptr = s;
+end_ptr = s;
+for(i = 0; i < (n - 1); i++ )
+{
+ch = *end_ptr;
+*end_ptr = *begin_ptr;
+*begin_ptr = ch;
+begin_ptr++;
+end_ptr--;
+}
 }
