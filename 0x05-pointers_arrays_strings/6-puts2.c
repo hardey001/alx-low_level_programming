f6566333a39c082a85c45bb748e381bd72773de7
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-14 17:58:43 -0700

    Update 6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,16 +12,11 @@ void puts2(char *str)
 int len = 0;
 while (str[len] != '\0')
 {
-len++;
-}
-len = len - 1;
-while ((len + 1) > 0)
-{
 if (len % 2 == 0)
 {
 _putchar(str[len]);
 }
-len--;
+len++;
 }
 _putchar ('\n');
 }
