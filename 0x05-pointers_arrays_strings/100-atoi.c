d3ce1544dd8a63b054f04ca432616b9885ceeb59
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-15 08:16:42 -0700

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,8 +11,15 @@ int _atoi(char *s)
 
 {
 
-int res = 0, i;
-for (i = 0; s[i] != '\0'; ++i)
+int res = 0;
+int sign = 1;
+int i = 0;
+if (s[0] == '-') 
+{
+sign = -1;
+i++;
+}
+for (; s[i] != '\0'; ++i)
 res = res * 10 + s[i] - '0';
-return res;
+return (sign * res);
 }
