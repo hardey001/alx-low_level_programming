7644fff40786bb86f6cdea2d97508d68f6301e2c
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-15 16:17:39 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <string.h>
 #include "main.h"
-
+/**
+*_strcat- retuns
+*@dest:char
+*@src: jud
+*Return: char
+*/
 char *_strcat(char *dest, char *src)
 {
 int le1, le2, i;
 le1 = strlen(*dest);
 le2 = strlen(*src);
-for (i = 0;i <= le2; i++)
+for (i = le1;i <= le2; i++)
 {
 dest[le1 + i] = src[i];
 }
+dest[le1 + i] = '\0'
 return (dest);
 }
