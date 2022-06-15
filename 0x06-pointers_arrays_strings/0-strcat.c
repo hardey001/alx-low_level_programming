028297ce223f7eb210f48b6fbf8131864ea9ef5a
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-15 16:20:53 -0700

    Update 0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,8 +10,8 @@
 char *_strcat(char *dest, char *src)
 {
 int le1, le2, i;
-le1 = strlen(*dest);
-le2 = strlen(*src);
+le1 = strlen(dest);
+le2 = strlen(src);
 for (i = le1;i <= le2; i++)
 {
 dest[le1 + i] = src[i];
