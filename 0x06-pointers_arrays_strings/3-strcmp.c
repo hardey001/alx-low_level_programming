3f4d092de998df3f975f9847d370e53bfcaa902a
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-16 06:32:25 -0700

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,11 +11,11 @@ int _strcmp(char *s1, char *s2)
 
 {
 int comp = 0;
-while ( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+while (( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2)
 {
 s1++;
 s2++;
 }
-comp = (*s1 == *s2) ? 0 : (*s1 > *s2) ?1 : -1;
-return comp;
+comp = (*s1 == *s2) ? 0 : (*s1 > *s2) ? 1 : -1;
+return (comp);
 }
