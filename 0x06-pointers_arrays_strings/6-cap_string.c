f6f6e1fdaea05c256a0734b00694f1f7d7fcb4fe
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-16 14:28:38 -0700

    caps

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*cap_string- retuns
+*@str:lol
+*Return: char
+*/
+
+char *cap_string(char *str)
+
+{	
+int i;
+for (i = 0; str[i] != '\0'; i++)
+{
+if ( i == 0)
+{
+if ((str[i] >= 'a' && str[i] <= 'z'))
+str[i] = str[i] - 32;
+continue;
+}
+if (str[i] == ' ' || str[i] == '.')
+{
+++i;
+if ((str[i] >= 'a' && str[i] <= 'z'))
+str[i] = str[i] - 32;
+continue;
+}
+}
+return (str);
+}
