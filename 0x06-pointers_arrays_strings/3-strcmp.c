9dcd226267f0091e238ef3b07da51a10e8b24cf2
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-16 06:12:06 -0700

    kolo

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,12 +10,17 @@
 int _strcmp(char *s1, char *s2)
 
 {
-int comp = 0;
-while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+int i;
+for (i = 0; s1[i] && s2[i]; ++i)
 {
-s1++;
-s2++;
+if (s1[i] == s2[i] || (s1[i] ^ 32) == s2[i])
+continue;
+else
+break;
 }
-comp = (*s1 == *s2) ? 0 : (*s1 > *s2) ?1 : -1;
-return comp;
+if (s1[i] == s2[i])
+return 0;
+if ((s1[i] | 32) < (s2[i] | 32))
+return -1;
+return 1;
 }
