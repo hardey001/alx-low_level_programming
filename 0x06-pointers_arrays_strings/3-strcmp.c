a4d77ed65f99fbd3068cd2a218ef1e4651d9d95e
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-15 20:33:40 -0700

    flying

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+/**
+*_strncpy- retuns
+*@dest:char
+*@src: jud
+*Return: int
+*/
+
+int _strcmp(char *s1, char *s2)
+{
+int comp = 0;
+while( ( *s1 != '\0' && *s2 != '\0' ) && *s1 == *s2 )
+{
+s1++;
+s2++;
+}
+comp = (*s1 ==*s2)?0:(*s1 >*s2)?1:-1;
+return comp;
+}
