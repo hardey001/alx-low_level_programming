ec7ccc03359880f269a4dd21788532207616accf
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-20 13:48:41 -0700

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,15 +1,17 @@
 #include <stdio.h>
 /**
- * _memset - memset
- * 
- * @s:
- * @b:
- * @n:
- */
-void _memset(char *s, char b, unsigned int n)
+* _memset - memset
+* 
+* @s:
+* @b:
+* @n:
+* Return: char
+*/
+char * _memset(char *s, char b, unsigned int n)
 {
 int i;
 char *st = (char*) s;
 for (i = 0; i < n; i++)
 st[i] = b;
+return(*st);
 }
