77fef411896810e103c2613a77376341f79e7cc5
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-21 10:19:56 -0700

    dtr

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strspn - jhhiueh
+ * @s:hhh
+ * @accept:uu 
+ * Return: int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{{
+unsigned int count = 0;
+for (int i = 0; s[i] != '\0'; i++)
+{
+for (int j = 0; accept[j] != '\0'; j++) 
+{
+if (s[i + j] == accept[j]) 
+{
+count = count + 1;
+break;
+}
+}
+}
+return (count);
+}
+}
