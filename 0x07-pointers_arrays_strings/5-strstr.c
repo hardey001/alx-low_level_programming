a14f1efcb88ab16d5b42b084ae56a367be204244
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-21 16:43:13 -0700

    iikf

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+#include "main.h"
+/**
+ * _strstr - ppoooi 
+ * 
+ * @haystack: k
+ *@needle: djdj
+ * Return: char*
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+int i;
+for (i=0; needle[i] && haystack[i]; i++)
+for (; needle[i] != haystack[i]; haystack++) 
+i=0;
+return needle[i] ? 0 : (char *)haystack;
+}
