ac00fb79eeadaa28d0662627ef8cf44294b13e09
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-20 23:31:49 -0700

    liok

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - jjfjjf 
+ * 
+ * @dest: jjgj
+ * @src: jdjdfjf
+ * @n: lflg
+ * Return: char* 
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+unsigned int i;
+char *csrc = src;
+char *cdest = dest;
+for (i = 0; i < n; i++)
+{
+cdest[i] = csrc[i];
+}
+return (dest);
+}
