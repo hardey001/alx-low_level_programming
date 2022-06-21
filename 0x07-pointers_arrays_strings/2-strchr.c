8f73d1e0ae63e88240c7128b9f79a09d836f0516
hardey001 <42822245+hardey001@users.noreply.github.com>
2022-06-21 09:11:28 -0700

    Update 2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,18 +1,18 @@
 #include "main.h"
 /**
  * _strchr - kklk
- * 
+ *
  * @s: loki
  * @c:kkdi
- * Return: char* 
+ * Return: char*
  */
+
 char *_strchr(char *s, char c)
+
 {
-while (*s != '\0')
-{ 
-if(*s != c)
-s++;
-else
-return s;	
-}
+do
+{
+if (*s == c) return s;
+} while (*s++) ;
+return (NULL);
 }
